4ad2f0ed8310f0586b038fb3931cd099cc150906
SasHiZosH <caio.5050@hotmail.com>
2020-07-07 22:47:30 -0300

    initial commit

---
+++ public/scripts.js
@@ -0,0 +1,16 @@
+const currentPage = location.pathname
+const menuItems = document.querySelectorAll("header .links a")
+
+for (item of menuItems) {
+  if (currentPage.includes(item.getAttribute("href"))) {
+    item.classList.add("active")
+  }
+}
+
+const formDelete = document.querySelector("#form-delete")
+formDelete.addEventListener("submit", function (event) {
+  const confirmation = confirm("Deseja Deletar?")
+  if (!confirmation) {
+    event.preventDefault()
+  }
+})
